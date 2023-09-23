87bbfb402ef9371a9b9d33c20cfe4309e8f82c82
Ibrahim Abdulhameed <ibdul1999@gmail.com>
2023-09-23 22:12:21 +0100

    FIX typo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -187,10 +187,7 @@
     property="twitter:description"
     content="Our tech hackathon is a melting pot of visionaries, and its purpose is as clear as day: to shape the future. Whether you're a coding genius, a design maverick, or a concept wizard, you'll have the chance to transform your ideas into reality. Solving real-world problems, pushing the boundaries of technology, and creating solutions that can change the world, that's what we're all about!"
   />
-  <meta
-    property="twitter:image"
-    content="{site_origin}/get-linked-banner.png"
-  />
+  <meta property="twitter:image" content="{site_origin}/getlinked-banner.png" />
 
   <!-- Meta Tags Generated with https://metatags.io -->
 </svelte:head>
